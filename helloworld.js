a4e21e9d92a4e6e66c55974ff7178fdb10f5675d
U Breu <ubreu@MacBook-UB.fritz.box>
2019-10-25 15:40:00 +0200

    initial commit

---
+++ helloworld.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.get('/', (req, res) => res.sendFile('index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
